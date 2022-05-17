c512cfa2a4d866046ed4eb03e0e74990dfdd6ad5
zanemaklin <zmaklin@gmail.com>
2022-05-17 14:29:08 -0400

    Part 1c

--- part1/Exercises/src/App.js
+++ part1/Exercises/src/App.js
@@ -1,72 +1,9 @@
 import React from 'react'
 
-
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
-}
-
-
-const Part = (props) => {
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>{props.parts.name} has {props.parts.exercises} exercises.</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part parts={props.course.parts[0]}/>
-      <Part parts={props.course.parts[1]}/>    
-      <Part parts={props.course.parts[2]}/>
-    </div>
-  )
-}
-
-
-const Total = (props) => {
-    
-  let b = 0
-  
-  for (let i = 0; i < props.course.parts.length; i++) {
-    b += props.course.parts[i].exercises;
-  }
-  
-  return (
-    <div>
-      <p>The Total number of exercises is {b}.</p> 
-    </div>
-  )
-}
-
-
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  
-  return (
-    <>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-    </>
+    <div>{counter}</div>
   )
 }
 
--- part1/Exercises/src/index.js
+++ part1/Exercises/src/index.js
@@ -1,7 +1,17 @@
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-)
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
