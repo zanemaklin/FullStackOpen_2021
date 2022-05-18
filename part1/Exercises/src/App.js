d43f4994b04b3a987ffcb43d6bc077dcf9ab832d
zanemaklin <zmaklin@gmail.com>
2022-05-18 13:25:37 -0400

    Passing state to child Components

--- part1/Exercises/src/App.js
+++ part1/Exercises/src/App.js
@@ -1,18 +1,50 @@
 import React from 'react'
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+  }
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
   
+  const decreaseByOne = () => setCounter(counter - 1)
+  
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+    <Display counter={counter}/>
+
+    <Button 
+      onClick={increaseByOne}
+      text='plus'
+    />
+
+    <Button 
+      onClick={setToZero}
+      text='zero'
+    />
+
+    <Button 
+      onClick={decreaseByOne}
+      text='minus'
+    />
+     </div>
   )
 }
 
