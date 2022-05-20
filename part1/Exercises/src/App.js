0f3530cf430a83674b064c6721c4f3332a084be6
zanemaklin <zmaklin@gmail.com>
2022-05-20 15:31:00 -0400

    Array Handling

--- part1/Exercises/src/App.js
+++ part1/Exercises/src/App.js
@@ -1,36 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => (
-    <button onClick={onClick}>
-      {text}
-    </button>
-)
-
-// Grouping left and right values into a single piece of state
-
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-   { setClicks({...clicks, left: clicks.left + 1})}
-  
-  
-  const handleRightClick = () => {
-    { setClicks({...clicks, right: clicks.right + 1})}
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
